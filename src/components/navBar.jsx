37e2f36ff6e3a32d206b8413212e591a1f14db46
Sergio Archila <archilargosergio@gmail.com>
2022-10-20 00:32:58 -0500

    [ADD] started to create components

---
+++ src/components/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navBar = () => {
+    return (
+        <div className='w-full h-10 bg-purple-500 place-content-center'>
+            <h3 className='text-center text-black'> Start </h3>
+        </div>
+    )
+}
+
+export default navBar;
