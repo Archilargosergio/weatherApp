f94d7f24791a1e594538770b5a948a4448477aeb
Sergio Archila <archilargosergio@gmail.com>
2022-10-27 22:48:39 -0500

    [ADD] Added menu icon and menu component into navBar

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div className='grid place-content-start mx-2 my-3'>
+            <ul className='text-white font-bold'>
+                <li><a href="/">Start</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import menuIcon from '../assets/menu_FILL0_wght400_GRAD0_opsz48.png';
+import Menu from './Menu';
 
 const NavBar = () => {
+    const [menu, setMenu] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(!menu);
+    }
     return (
         <div className='flex w-full h-10 bg-slate-800 place-content-center'>
-            <h3 className='pt-2 text-center text-white font-bold'>  APLICATION WEATHER </h3>
+            <div className='w-10 mx-2 h-auto hover:cursor-pointer'>
+                <img className='w-full' src={menuIcon} alt="menu" onClick={handleMenu} />
+                {menu && < Menu />}
+            </div>
+            <h3 className='w-full h-auto pt-2 mr-16 text-center text-white font-bold'> APLICATION WEATHER </h3>
         </div>
     )
 }
